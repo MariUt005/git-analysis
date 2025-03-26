fa3a78ff139856b542759417fadad2cd76b05461
Maria <maria.ut.005@yandex.ru>
2025-03-26 22:38:01 +0300

    Git commit history

--- ETL.R
+++ ETL.R
@@ -1,5 +1,8 @@
 library(glue) # Для вызова системных команд
 library(dplyr)
+library(arrow)
+
+repo_url <- "https://github.com/tidyverse/ggplot2.git"
 
 # Клонирует репозиторий во временную или в указанную директорию
 # Если директория не пуста, обновляет информацию про репозиторий
@@ -8,7 +11,6 @@ getRepo <- function(repo_url, dir_parent){
     dir_parent <- tempdir()
   }
   dir <- strsplit(repo_url, "//")[[1]][2]
-  #gsub("/", "\\\\", dir)
   dir <- file.path(dir_parent, dir)
   print(dir)
   if (file.exists(dir)) {
@@ -22,7 +24,17 @@ getRepo <- function(repo_url, dir_parent){
   return(dir)
 }
 
-dir_repo <- getRepo("https://github.com/tidyverse/ggplot2.git")
-#list.files(dir_repo)
-#print(dir_repo)
+repo_dir <- getRepo(repo_url)
 
+git_log_cmd <- glue('git -C {repo_dir} log --format="%H\t%P\t%an\t%ai\t%s" --all')
+res <- system(git_log_cmd, intern = TRUE)
+
+git_commit_history <- data.frame(lines=res) %>%
+  tidyr::separate(
+    col = .data$lines,
+    into = c("commitHash", "parentHash", "author", "date", "message"),
+    sep = '\t'
+    )
+
+write_parquet(git_commit_history, paste(gsub("/", "_", strsplit(repo_url, "//")[[1]][2]),".parquet", sep=""))
+test <- read_parquet(paste(gsub("/", "_", strsplit(repo_url, "//")[[1]][2]),".parquet", sep=""))
