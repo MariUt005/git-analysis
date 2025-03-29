2d49d9c9dd7d77f91c906869548ae799938cc99d
Maria <maria.ut.005@yandex.ru>
2025-03-30 00:29:50 +0300

    Changes in files

--- ETL.R
+++ ETL.R
@@ -12,13 +12,10 @@ getRepo <- function(repo_url, dir_parent){
   }
   dir <- strsplit(repo_url, "//")[[1]][2]
   dir <- file.path(dir_parent, dir)
-  print(dir)
   if (file.exists(dir)) {
     git <- glue("git -C {dir} pull {repo_url}")
-    print("pull")
   } else {
     git <- glue("git clone {repo_url} {dir}")
-    print("clone")
   }
   system(git)
   return(dir)
@@ -37,4 +34,58 @@ git_commit_history <- data.frame(lines=res) %>%
     )
 
 write_parquet(git_commit_history, paste(gsub("/", "_", strsplit(repo_url, "//")[[1]][2]),".parquet", sep=""))
+
+git_diff_cmd <- glue('git -C {repo_dir} log -p --unified=0 -w --ignore-blank-lines')
+diff <- system(git_diff_cmd, intern = TRUE)
+
+git_diff_df <- data.frame(lines=diff)
+df <- git_diff_df %>%
+  filter(!grepl('^Author', lines)) %>%
+  filter(!grepl('^Date', lines)) %>%
+  filter(!grepl('^ ', lines)) %>%
+  filter(!grepl('^diff', lines)) %>%
+  filter(!grepl('^index', lines)) %>%
+  filter(lines != "")
+
+i = 1
+n = nrow(df)
+changesDf <- data.frame(commitHash= numeric(0), fileA= numeric(0), fileB= numeric(0), fromFileRange= numeric(0), toFileRange= numeric(0), isAdd= numeric(0), text= numeric(0))
+j = 1
+while (i < n) {
+  commit = NA
+  fileA = NA
+  fileB = NA
+  fromFileRange = NA
+  toFileRange = NA
+  isAdd = NA
+  text = NA
+  if (grepl("^commit", df$lines[i])) {
+    commit <- strsplit(df$lines[i], " ")[[1]][2]
+    i = i + 1
+  }
+  while (!grepl("^commit", df$lines[i])) {
+    if (grepl("^---", df$lines[i])) {
+      fileA <- substring(strsplit(df$lines[i], " ")[[1]][2], 3)
+      i = i + 1
+    }
+    if (grepl("^\\+\\+\\+", df$lines[i])) {
+      fileB <- substring(strsplit(df$lines[i], " ")[[1]][2], 3)
+      i = i + 1
+    }
+    if (grepl("^@@", df$lines[i])) {
+      fromFileRange <- strsplit(df$lines[i], " ")[[1]][2]
+      toFileRange <- strsplit(df$lines[i], " ")[[1]][3]
+      i = i + 1
+    }
+    while ((grepl("^-", df$lines[i]) | grepl("^\\+", df$lines[i])) & !(grepl("^---", df$lines[i]) | grepl("^\\+\\+\\+", df$lines[i]))) {
+      isAdd = grepl("^\\+", df$lines[i])
+      text = substring(df$lines[i], 2)
+      changesDf[j, ] <- c(commit, fileA, fileB, fromFileRange, toFileRange, isAdd, text)
+      i = i + 1
+      j = j + 1
+    }
+  }
+  print(j)
+}
+
 test <- read_parquet(paste(gsub("/", "_", strsplit(repo_url, "//")[[1]][2]),".parquet", sep=""))
